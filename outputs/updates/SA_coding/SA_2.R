db29b266d798f73303ecbd8f8ccb43a5b7961535
Ailli3 <jingningyao@gmail.com>
2024-05-28 17:24:49 +1000

    1) solve the permutations generating function. 2) change the temperature function. 3) Solution doesn't really coverage due to runif(1) function.

--- outputs/updates/SA_coding/SA_2.R
+++ outputs/updates/SA_coding/SA_2.R
@@ -3,8 +3,8 @@ library(tidyverse)
 P_matrix <- function(Z, R, G) {
   R_inv <- solve(R)
   G_inv <- solve(G)
-  ZtR_invZ <- t(Z) %*% R_inv %*% Z
-  middle_inv <- solve(ZtR_invZ + G_inv) 
+  XtR_invZ <- t(Z) %*% R_inv %*% Z
+  middle_inv <- solve(XtR_invZ + G_inv) 
   P <- R_inv - R_inv %*% Z %*% middle_inv %*% t(Z) %*% R_inv
   return(P)
 }
@@ -16,26 +16,36 @@ a_criterion_r <- function(X, D, P){
   return(sum(diag(lambda)))
 }
 
-a_criterion_f <- function(Xb, Zt){
-  X <- cbind(Xb, Zt)
+#？
+a_criterion_f <- function(Xb, Xt){
+  X <- cbind(Xb, Xt)
   information_matrix <- t(X) %*% X
-  cols_Zt <- ncol(Zt)
-  Zt_information <- information_matrix[(nrow(information_matrix) - cols_Zt + 1):nrow(information_matrix), 
-                                       (ncol(information_matrix) - cols_Zt + 1):ncol(information_matrix)]
-  Zt_information_inv <- solve(Zt_information)
-  return(sum(diag(Zt_information_inv)))
+  cols_Xt <- ncol(Xt)
+  Xt_information <- information_matrix
+  Xt_information_inv <- MASS::ginv(Xt_information)[(nrow(information_matrix) - cols_Xt + 1):nrow(information_matrix), 
+                                              (ncol(information_matrix) - cols_Xt + 1):ncol(information_matrix)]
+  return(sum(diag(Xt_information_inv)))
+}
+#？
+is_matrix_in_list <- function(matrix, list_of_matrices) {
+  for (mat in list_of_matrices) {
+    if (identical(matrix, mat)) {
+      return(TRUE)
+    }
+  }
+  return(FALSE)
 }
 
 
-generate_all_neighbors <- function(X) {
+generate_all_permutations <- function(X) {
   n <- nrow(X)
   neighbors <- list()  # initialize the list
   index <- 1  # index
   
   for (i in 1:(n-1)) {
     for (j in (i+1):n) {
-      X_new <- X  # create a copy of X
-      # Swap rows i and j
+      X_new <- X
+      
       temp <- X_new[i, ]  
       X_new[i, ] <- X_new[j, ]
       X_new[j, ] <- temp
@@ -44,6 +54,33 @@ generate_all_neighbors <- function(X) {
       index <- index + 1
     }
   }
+  return(neighbors)
+}
+
+generate_pairwise_permutations <- function(X) {
+  n <- nrow(X)
+  neighbors <- list()
+  index <- 1
+  
+  neighbors[[index]] <- X
+  index <- index + 1
+  
+  for (i in 1:(n - 1)) {
+    for (j in (i + 1):n) {
+      X_new <- X
+      temp <- X_new[i, ]
+      X_new[i, ] <- X_new[j, ]
+      X_new[j, ] <- temp
+      
+      if (!is_matrix_in_list(X_new, neighbors)) {
+        neighbors[[index]] <- X_new
+        index <- index + 1
+      }
+    }
+  }
+  
+  neighbors <- neighbors[-1]
+  
   return(neighbors)
 }
 
@@ -56,69 +93,80 @@ is_matrix_in_list <- function(matrix, list_of_matrices) {
   return(FALSE)
 }
 
-simulated_annealing <- function(data, R, max_iter,cooling_rate, epsilon, if_fix = TRUE) {
-  if (if_fix == TRUE){
-    X <- model.matrix(~-1+col+row + trt, data = data)
+simulated_annealing <- function(data,
+                                R = diag(0.1, nrow = nrow(data), ncol = nrow(data)),
+                                max_iter = 10000,
+                                initial_temperature = 100,
+                                cooling_rate = 0.01,
+                                epsilon = 0.00000001,
+                                fixed_treatment = TRUE) {
+  if (fixed_treatment == TRUE){
+    X <- model.matrix(~ -1 + col + row + trt, data = data)
     Xb <- model.matrix(~ -1 + col + row, data = data)
-    Zt <- model.matrix(~ -1 + trt, data = data)
-  
-    Zt_current <- Zt
-    a_current <- a_criterion_f(Xb, Zt)
-    a_values <- numeric()
-    Zt_list <- list()
-  
-    i=1
-    index <- 1
-    while (i <= max_iter) {
-      temperature <- max_iter / (i * cooling_rate)
-      Zt_candidate <- generate_all_neighbors(Zt_current)
-      for (candidate in Zt_candidate ){
+    Xt <- model.matrix(~ -1 + trt, data = data)
+    Xt_current <- Xt
+    a_current <- a_criterion_f(Xb, Xt)
+    a_values <- numeric(length = max_iter)
+    a_values[1] <- a_current
+    Xt_list <- list()
+    Xt_list[[1]] <- Xt_current
+    i <- 1
+    while (i == 1 || 
+           (i <= max_iter && 
+                      abs(a_values[i-1] - a_current) / a_values[i-1] > epsilon)) {
+      temperature <- initial_temperature * exp(-cooling_rate * i)
+      Xt_candidate <- generate_pairwise_permutations(Xt_current)
+      for (candidate in Xt_candidate ){
         a_candidate <- a_criterion_f(Xb, candidate)
-        if ((a_candidate < a_current || runif(1) < exp((a_current - a_candidate)/temperature)) && !is_matrix_in_list(candidate,Zt_list )){
-          print("we change the design matrix")
-          Zt_current <- candidate
+        if ((a_candidate < a_current 
+             || runif(1) < exp(-abs(a_current - a_candidate)/temperature)) && !is_matrix_in_list(candidate,Xt_list )
+            ){
+          message("we change the design matrix")
+          Xt_current <- candidate
           a_current <- a_candidate
           break
         }
       }
-      if (length(a_values) >= 2 && a_values[length(a_values)] != 0 && abs(a_values[length(a_values)] - a_current) / a_values[length(a_values)] <= epsilon){
-        break
-      }
-      Zt_list[[index]] <- Zt_current
-      a_values <- c(a_values, a_current)
-      index <- index+1
       i <- i+1
+      Xt_list[[i]] <- Xt_current
+      a_values[i] <- a_current
     }
   
-  return(list(optimal_design = Zt_current, a_history = a_values, iteration_number = i))
+  return(list(optimal_design = Xt_current, a_history = a_values, iteration_number = i))
   }
+
 }
 
 data <- expand.grid(col=as.factor(1:4),
                     row=as.factor(1:4)) |>
   mutate(trt = sample(rep(LETTERS[1:4], 4)))
-
+#data
 X <- model.matrix(~-1+col+row + trt, data = data)
 R <- diag(0.1, nrow=16, ncol=16)
 
-optimal_design_info <- simulated_annealing(data, R, 10000, 1, 0.00000001, if_fix = TRUE)
-X
+optimal_design_info <- simulated_annealing(data, R)
 
 Xb <- model.matrix(~ -1 + col + row, data = data)
-Zt <- model.matrix(~ -1 + trt, data = data)
+Xt <- model.matrix(~ -1 + trt, data = data)
+Xt
 
 
-Zt_candidate <- generate_all_neighbors(Zt)
-for (candidate in Zt_candidate ){
-  print(a_criterion_f(Xb,candidate))
-}
 #optimal_design_matrix <- as.matrix(optimal_design_info[["optimal_design"]])
+
+iteration_number <- optimal_design_info[["iteration_number"]]
+
 #optimal_design_matrix
+
+a_values_vector <- unlist(optimal_design_info['a_history'])
+par(mar=c(5.1, 4.1, 4.1, 2.1))
+plot(a_values_vector[1:iteration_number], type = "l", main = "A-criterion over Iterations", xlab = "Iteration", ylab = "A-criterion", col = "blue")
+
 #Xb <- model.matrix(~ -1 + col + row, data = data)
-#Zt <- model.matrix(~ -1 + trt, data = data)
+#Xt <- model.matrix(~ -1 + trt, data = data)
 #Xb
-#Zt
+#Xt
 
-
+X1 <- generate_all_permutations(Xt)
+X2 <- generate_pairwise_permutations(Xt)
 
 
