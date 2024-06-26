ee218cb5d55e6a29200594280f1940cf9940c213
Emi Tanaka <dr.emi.tanaka@gmail.com>
2024-06-26 15:37:08 +1000

    pkg

---
+++ optrowcoldesign/R/optrowcoldesign-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+#' @importFrom stats model.matrix
+#' @importFrom tidyselect eval_select
+#' @import rlang
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
